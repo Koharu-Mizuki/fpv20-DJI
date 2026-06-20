cc0ea8f1bf8a72a65fe15200fc723c19d3e272e2
Akiyama-Mizuki-44 <w_mutsumi@outlook.com>
2026-06-21 00:09:38 +0800

    style: 润色中文翻译，统一术语与措辞
    OSD→摇杆图、校准方法措辞规范化、rate→灵敏度、信道→通道、
    界面标题改为「FPV 无人机设置」等。

--- src/main/java/com/iung/fpv20/generate/lang/ChineseLangProvider.java
+++ src/main/java/com/iung/fpv20/generate/lang/ChineseLangProvider.java
@@ -14,26 +14,26 @@ public class ChineseLangProvider extends FabricLanguageProvider {
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add(ModBlocks.RECEIVER_BLOCK,"信号接收器");
 
-        translationBuilder.add(TranslateKeys.BTN_OPTION_ENTRY,"FPV");
-        translationBuilder.add(TranslateKeys.TITLE_MAIN_OPTION_SCREEN,"FPV选项");
+        translationBuilder.add(TranslateKeys.BTN_OPTION_ENTRY,"FPV 无人机");
+        translationBuilder.add(TranslateKeys.TITLE_MAIN_OPTION_SCREEN,"FPV 无人机设置");
         translationBuilder.add(TranslateKeys.BTN_SELECT_CONTROLLER,"选择控制器");
         translationBuilder.add(TranslateKeys.BTN_SELECT,"选择");
-        translationBuilder.add(TranslateKeys.RATE_1,"rate:%s");
+        translationBuilder.add(TranslateKeys.RATE_1,"灵敏度: %s");
         translationBuilder.add(TranslateKeys.BTN_CALIBRATE,"校准");
 
-        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_CENTER,"校准中心");
-        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_RANGE,"校准范围");
+        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_CENTER,"校准摇杆中点");
+        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_RANGE,"校准摇杆行程");
 
         translationBuilder.add(TranslateKeys.BTN_START,"开始");
         translationBuilder.add(TranslateKeys.BTN_END,"结束");
 
         translationBuilder.add(TranslateKeys.BTN_OPTION,"选项");
 
-        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_MAX_MIN,"校准方法:最大最小值");
-        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_MAX_MID_MIN,"校准方法:最大零点最小");
-        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_RAW,"校准方法:原始");
+        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_MAX_MIN,"校准方法: 最大-最小");
+        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_MAX_MID_MIN,"校准方法: 最大-中点-最小");
+        translationBuilder.add(TranslateKeys.BTN_CALIBRATE_RAW,"校准方法: 直通");
 
-        translationBuilder.add(TranslateKeys.TEXT_SET_CHANNEL_NAME,"设置信道名称");
+        translationBuilder.add(TranslateKeys.TEXT_SET_CHANNEL_NAME,"设置通道名称");
 
         translationBuilder.add(TranslateKeys.BTN_SHOW_INPUT,"显示输入信号");
         translationBuilder.add(TranslateKeys.BTN_BTN_CONFIG,"按钮或开关设置");
@@ -43,10 +43,10 @@ public class ChineseLangProvider extends FabricLanguageProvider {
 
         translationBuilder.add(TranslateKeys.BTN_RELOAD_CONFIG,"重新加载配置");
 
-        translationBuilder.add(TranslateKeys.BTN_OSD_ON, "显示OSD: 是的");
-        translationBuilder.add(TranslateKeys.BTN_OSD_OFF, "显示OSD: 不");
-        translationBuilder.add(TranslateKeys.KEYBINDS_CATEGORY, "fpv20");
-        translationBuilder.add(TranslateKeys.KEYBIND_OSD, "切换OSD");
+        translationBuilder.add(TranslateKeys.BTN_OSD_ON, "显示摇杆图: 开");
+        translationBuilder.add(TranslateKeys.BTN_OSD_OFF, "显示摇杆图: 关");
+        translationBuilder.add(TranslateKeys.KEYBINDS_CATEGORY, "FPV无人机");
+        translationBuilder.add(TranslateKeys.KEYBIND_OSD, "切换摇杆图显示");
     }
 
 
